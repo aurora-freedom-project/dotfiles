c7a25b34f772d545e5e01e9063d511580ac3ddd7
RnD <your-email@example.com>
2025-04-14 10:20:29 +0700

    Moving home-manager from stable to unstable package on nixpkgs

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     # Home manager
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     
     # Nix-darwin for macOS
